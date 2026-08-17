30230252c00143d67da4e4ee51824aef1e78084f
Dablio Piza da Silva <dablio.p.silva@unesp.br>
2026-08-16 23:12:54 -0300

    menu principal e menu lateral adicionado

---
+++ js.js
@@ -0,0 +1,9 @@
+function abrirMenu() {
+            document.getElementById('menuLateral').classList.add('ativo');
+            document.getElementById('fundoEscuro').classList.add('ativo');
+        }
+
+        function fecharMenu() {
+            document.getElementById('menuLateral').classList.remove('ativo');
+            document.getElementById('fundoEscuro').classList.remove('ativo');
+        }
